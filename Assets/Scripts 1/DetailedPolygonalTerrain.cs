6cfcb58059082b63a641e26e8e746ea9e381d2b9
Sleepy <bluhrotbrain@proton.me>
2025-05-11 16:59:04 +0200

    New player scripts

--- Assets/Scripts 1/DetailedPolygonalTerrain.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c580d4e04b08e639005c1e78b8d7bb0eddf1e1333002a75e82dede440bac962c
-size 8870
--- Assets/Scripts 1/PolygonalTerrainGenerator.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f539ff74e7a0f71a6e42717a159806d4b5916d7efda8e8126dd8f3c1dee3c840
-size 5421
